7efd52e54d93e529707c5a9b25ff01a56d2e74f1
Theocharis Pasvantis <xaroulan17@gmail.com>
2024-03-05 03:32:39 +0200

    fix: bigger dividers

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -12,7 +12,7 @@ function Section({ title, text, index }) {
       >
         <div className="flex items-center w-full">
           <h2 className="mr-3 text-2xl font-semibold">{title}</h2>
-          <div className="flex-grow h-[2px] rounded-full bg-black/20 dark:bg-white/20" />
+          <div className="flex-grow h-[4px] rounded-full bg-black/20 dark:bg-white/20" />
         </div>
         <p className="mt-[10px]">{text}</p>
       </motion.section>
